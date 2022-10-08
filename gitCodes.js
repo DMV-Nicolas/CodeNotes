5e06c95c51dd21eb68c71d308fd1c16242793d72
dmvnicolas <dmvnicolas@gmail.com>
2022-10-08 16:05:46 -0500

    Commit inicial

---
+++ gitCodes.js
@@ -0,0 +1,4 @@
+'git status' // Ver el estado de los archivos
+'git init' // Inicializar repositorio
+'git add ARCHIVOS' // Agregar archivos al stage
+'git commit -m "info"' // Comprometer los archivos del stage
---
+++ npmCodes.js
@@ -0,0 +1,1 @@
+'npm list -g -depth 0' // Listar modulos globales instalados
